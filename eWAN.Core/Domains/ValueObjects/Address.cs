b96cda86f2888befb9d18a083ac5ba8437bfc58d
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2020-03-21 18:27:41 +0800

    refactor: Make Value Objects immutable

--- eWAN.Core/Domains/ValueObjects/Address.cs
+++ eWAN.Core/Domains/ValueObjects/Address.cs
@@ -4,12 +4,36 @@ namespace eWAN.Core.Domains.ValueObjects
 
     public class Address : ValueObject
     {
-        public string street { get; set; }
-        public string barangay { get; set; }
-        public string subdivision { get; set; }
-        public string city { get; set; }
-        public string region { get; set; }
-        public string zip { get; set; }
+        public string street { get; private set; }
+        public string barangay { get; private set; }
+        public string subdivision { get; private set; }
+        public string city { get; private set; }
+        public string region { get; private set; }
+        public string zip { get; private set; }
+
+        private Address()
+        {}
+        public Address(
+            string street,
+            string barangay,
+            string subdivision,
+            string city,
+            string region,
+            string zip
+        )
+        {
+            this.street = street;
+            this.barangay = barangay;
+            this.subdivision = subdivision;
+            this.city = city;
+            this.region = region;
+            this.zip = zip;
+        }
+
+        public override string ToString()
+        {
+            return $"{street}, {barangay}, {subdivision}, {city}, {region}, {zip}";
+        }
 
         protected override IEnumerable<object> GetAtomicValues()
         {
--- eWAN.Core/Domains/ValueObjects/Identity.cs
+++ eWAN.Core/Domains/ValueObjects/Identity.cs
@@ -4,9 +4,32 @@ namespace eWAN.Core.Domains.ValueObjects.Identity
 
     public class UserDetails : ValueObject
     {
-        public string firstName { get; set; }
-        public string middleName { get; set; }
-        public string lastName { get; set; }
+        public string firstName { get; private set; }
+        public string middleName { get; private set; }
+        public string lastName { get; private set; }
+
+        private UserDetails()
+        {}
+        public UserDetails(
+            string firstName,
+            string middleName,
+            string lastName
+        )
+        {
+            this.firstName = firstName;
+            this.middleName = middleName;
+            this.lastName = lastName;
+        }
+
+        public string GetProperFullName()
+        {
+            return $"{firstName} ${middleName} ${lastName}";
+        }
+
+        public string GetFormalFullName()
+        {
+            return $"{lastName}, {firstName} {middleName}";
+        }
 
         protected IEnumerable<object> GetAtomicValues()
         {
@@ -18,9 +41,23 @@ namespace eWAN.Core.Domains.ValueObjects.Identity
 
     public class UserContacts : ValueObject
     {
-        public string emailAddress { get; set; }
-        public string mobileNumber { get; set; }
-        public Address homeAddress { get; set; }
+        public string emailAddress { get; private set; }
+        public string mobileNumber { get; private set; }
+        public Address homeAddress { get; private set; }
+
+        private UserContacts()
+        {}
+        
+        public UserContacts(
+            string emailAddress,
+            string mobileNumber,
+            Address homeAddress
+        )
+        {
+            this.emailAddress = emailAddress;
+            this.mobileNumber = mobileNumber;
+            this.homeAddress = homeAddress;
+        }
 
         protected IEnumerable<object> GetAtomicValues()
         {
@@ -32,8 +69,20 @@ namespace eWAN.Core.Domains.ValueObjects.Identity
 
     public class UserGuardian : ValueObject
     {
-        public string name { get; set; }
-        public string mobileNumber { get; set; }
+        public string name { get; private set; }
+        public string mobileNumber { get; private set; }
+
+        private UserGuardian()
+        {}
+
+        public UserGuardian(
+            string name,
+            string mobileNumber
+        )
+        {
+            this.name = name;
+            this.mobileNumber = mobileNumber;
+        }
 
         protected IEnumerable<object> GetAtomicValues()
         {

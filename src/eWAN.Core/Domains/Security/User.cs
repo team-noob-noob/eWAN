6de8de04d33de3874ff9c3fc5dd8aeab7f6826c7
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2020-04-11 20:00:48 +0800

    feat: Implement IUser

---
+++ src/eWAN.Core/Domains/Security/User.cs
@@ -0,0 +1,12 @@
+using eWAN.Core.Domains.Account;
+
+namespace eWAN.Core.Domains.Security
+{
+    public class User : BaseEntity, IUser
+    {
+        public AccountId accountId { get; protected set; }
+        public ExternalUserId externalUserId { get; protected set; }
+        public string username { get; protected set; }
+        public string password { get; protected set; }
+    }
+}

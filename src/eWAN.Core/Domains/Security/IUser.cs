f54a74781bfc03b8f9b0c642e5e8565ac21f2c7e
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2020-04-11 20:00:29 +0800

    chore: Add ids

--- src/eWAN.Core/Domains/Security/IUser.cs
+++ src/eWAN.Core/Domains/Security/IUser.cs
@@ -1,7 +1,11 @@
+using eWAN.Core.Domains.Account;
+
 namespace eWAN.Core.Domains.Security
 {
     public interface IUser
     {
+        AccountId accountId { get; }
+        ExternalUserId externalUserId { get; }
         string username { get; }
         string password { get; }
     }

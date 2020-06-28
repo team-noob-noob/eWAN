0d95c7e9957fa4d6860f756534162ea4107de0fe
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2020-06-28 20:02:46 +0800

    fix: Email not required in registration

--- WebApi/UseCases/Register/RegisterRequest.cs
+++ WebApi/UseCases/Register/RegisterRequest.cs
@@ -10,6 +10,7 @@ namespace eWAN.WebApi.UseCases.Register
         [Required]
         public string Password { get; set; } = string.Empty;
 
+        [Required]
         public string Email { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
 

6f570ab0c59b1089608fc96f15385fd9aff67abd
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2020-07-10 23:10:27 +0800

    refactor: allow write in update and delete

--- Domains/IBaseEntity.cs
+++ Domains/IBaseEntity.cs
@@ -5,7 +5,7 @@ namespace eWAN.Domains
     public interface IBaseEntity
     {
         public DateTime createdAt { get; }
-        public DateTime? updatedAt { get; }
-        public DateTime? deletedAt { get; }
+        public DateTime? updatedAt { get; set; }
+        public DateTime? deletedAt { get; set; }
     }
 }

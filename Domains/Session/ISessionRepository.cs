2e7aae1c0c1c670cce708fa94fc02c98d48c6fbc
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2020-07-12 15:16:00 +0800

    feat: Add queries to get sessions

--- Domains/Session/ISessionRepository.cs
+++ Domains/Session/ISessionRepository.cs
@@ -1,9 +1,17 @@
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace eWAN.Domains.Session
 {
+    using Room;
+    using User;
+    using Semester;
+
     public interface ISessionRepository
     {
         Task Add(ISession session);
+        Task<ICollection<ISession>> GetSessionsByRoomAndSemester(IRoom room, ISemester semester);
+        Task<ICollection<ISession>> GetSessionsByInstructorAndSemester(IUser instructor, ISemester semester);
+        Task<ICollection<ISession>> GetSessionsByStudentAndSemester(IUser student, ISemester semester);
     }
 }
--- Infrastructure/Database/Repositories/SessionRepository.cs
+++ Infrastructure/Database/Repositories/SessionRepository.cs
@@ -1,8 +1,13 @@
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace eWAN.Infrastructure.Database.Repositories
 {
     using Domains.Session;
+    using eWAN.Domains.Room;
+    using eWAN.Domains.Semester;
+    using eWAN.Domains.User;
     using Session = Entities.Session;
 
     public class SessionRepository : ISessionRepository
@@ -14,6 +19,30 @@ namespace eWAN.Infrastructure.Database.Repositories
         {
             this._context.Add((Session) session);
             await this._context.SaveChangesAsync();
+        }
+
+        public async Task<ICollection<ISession>> GetSessionsByRoomAndSemester(IRoom room, ISemester semester)
+        {
+            var sessions = semester.OpenCourses
+                .SelectMany(x => x.Sessions)
+                .Where(y => y.Room.Id == room.Id);
+            return await Task.FromResult(sessions.ToHashSet());
+        }
+
+        public async Task<ICollection<ISession>> GetSessionsByInstructorAndSemester(IUser instructor, ISemester semester)
+        {
+            var sessions = semester.OpenCourses
+                .SelectMany(x => x.Sessions)
+                .Where(y => y.Instructor.Id == instructor.Id);
+            return await Task.FromResult(sessions.ToHashSet());
+        }
+
+        public async Task<ICollection<ISession>> GetSessionsByStudentAndSemester(IUser student, ISemester semester)
+        {
+            var sessions = semester.OpenCourses
+                .Where(x => x.Students.Where(y => y.enrolledStudent.Id == student.Id).Count() >= 1)
+                .SelectMany(x => x.Sessions);
+            return await Task.FromResult(sessions.ToHashSet());
         }
     }
 }

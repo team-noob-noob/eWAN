322d7a3f02dbb8b7a2fdd5bc9fd137a66a33d30c
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2020-08-09 00:55:02 +0800

    chore(EF/Migrations): Add new migration

---
+++ Infrastructure/Database/Migrations/20200808165439_fixedFixedInitialMigration.cs
@@ -0,0 +1,433 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace Infrastructure.Migrations
+{
+    public partial class fixedFixedInitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Programs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    createdAt = table.Column<DateTime>(nullable: false),
+                    updatedAt = table.Column<DateTime>(nullable: true),
+                    deletedAt = table.Column<DateTime>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Programs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    createdAt = table.Column<DateTime>(nullable: false),
+                    updatedAt = table.Column<DateTime>(nullable: true),
+                    deletedAt = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sections",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    createdAt = table.Column<DateTime>(nullable: false),
+                    updatedAt = table.Column<DateTime>(nullable: true),
+                    deletedAt = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sections", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Semesters",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    createdAt = table.Column<DateTime>(nullable: false),
+                    updatedAt = table.Column<DateTime>(nullable: true),
+                    deletedAt = table.Column<DateTime>(nullable: true),
+                    Start = table.Column<DateTime>(nullable: false),
+                    End = table.Column<DateTime>(nullable: false),
+                    IsOpenForEnrollment = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Semesters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    createdAt = table.Column<DateTime>(nullable: false),
+                    updatedAt = table.Column<DateTime>(nullable: true),
+                    deletedAt = table.Column<DateTime>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    ParentCourse_Id = table.Column<string>(nullable: true),
+                    Program_Id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Courses_Courses_ParentCourse_Id",
+                        column: x => x.ParentCourse_Id,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Courses_Programs_Program_Id",
+                        column: x => x.Program_Id,
+                        principalTable: "Programs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    createdAt = table.Column<DateTime>(nullable: false),
+                    updatedAt = table.Column<DateTime>(nullable: true),
+                    deletedAt = table.Column<DateTime>(nullable: true),
+                    AssignedSectionId = table.Column<int>(nullable: true),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    MiddleName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_User_Sections_AssignedSectionId",
+                        column: x => x.AssignedSectionId,
+                        principalTable: "Sections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    createdAt = table.Column<DateTime>(nullable: false),
+                    updatedAt = table.Column<DateTime>(nullable: true),
+                    deletedAt = table.Column<DateTime>(nullable: true),
+                    CourseId = table.Column<string>(nullable: true),
+                    SemesterId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subjects_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Subjects_Semesters_SemesterId",
+                        column: x => x.SemesterId,
+                        principalTable: "Semesters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Applications",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    createdAt = table.Column<DateTime>(nullable: false),
+                    updatedAt = table.Column<DateTime>(nullable: true),
+                    deletedAt = table.Column<DateTime>(nullable: true),
+                    isAccepted = table.Column<bool>(nullable: false),
+                    reason = table.Column<string>(nullable: true),
+                    Applicant_Id = table.Column<string>(nullable: false),
+                    Staff_Id = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Applications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Applications_User_Applicant_Id",
+                        column: x => x.Applicant_Id,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Applications_User_Staff_Id",
+                        column: x => x.Staff_Id,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EnrolledProgram",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    createdAt = table.Column<DateTime>(nullable: false),
+                    updatedAt = table.Column<DateTime>(nullable: true),
+                    deletedAt = table.Column<DateTime>(nullable: true),
+                    Student_Id = table.Column<string>(nullable: true),
+                    Program_Id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnrolledProgram", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EnrolledProgram_Programs_Program_Id",
+                        column: x => x.Program_Id,
+                        principalTable: "Programs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EnrolledProgram_User_Student_Id",
+                        column: x => x.Student_Id,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRoles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    createdAt = table.Column<DateTime>(nullable: false),
+                    updatedAt = table.Column<DateTime>(nullable: true),
+                    deletedAt = table.Column<DateTime>(nullable: true),
+                    role = table.Column<int>(type: "int", nullable: false),
+                    User_Id = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRoles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserRoles_User_User_Id",
+                        column: x => x.User_Id,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EnrolledSubjects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    createdAt = table.Column<DateTime>(nullable: false),
+                    updatedAt = table.Column<DateTime>(nullable: true),
+                    deletedAt = table.Column<DateTime>(nullable: true),
+                    grade = table.Column<string>(nullable: true),
+                    Subject_Id = table.Column<string>(nullable: true),
+                    User_Id = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnrolledSubjects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EnrolledSubjects_Subjects_Subject_Id",
+                        column: x => x.Subject_Id,
+                        principalTable: "Subjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_EnrolledSubjects_User_User_Id",
+                        column: x => x.User_Id,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    createdAt = table.Column<DateTime>(nullable: false),
+                    updatedAt = table.Column<DateTime>(nullable: true),
+                    deletedAt = table.Column<DateTime>(nullable: true),
+                    RoomId = table.Column<string>(nullable: true),
+                    InstructorId = table.Column<string>(nullable: true),
+                    SubjectId = table.Column<string>(nullable: true),
+                    Day = table.Column<int>(nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    StartTime = table.Column<TimeSpan>(nullable: false),
+                    EndTime = table.Column<TimeSpan>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sessions_User_InstructorId",
+                        column: x => x.InstructorId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Sessions_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Sessions_Subjects_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_Applicant_Id",
+                table: "Applications",
+                column: "Applicant_Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_Staff_Id",
+                table: "Applications",
+                column: "Staff_Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_ParentCourse_Id",
+                table: "Courses",
+                column: "ParentCourse_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_Program_Id",
+                table: "Courses",
+                column: "Program_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnrolledProgram_Program_Id",
+                table: "EnrolledProgram",
+                column: "Program_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnrolledProgram_Student_Id",
+                table: "EnrolledProgram",
+                column: "Student_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnrolledSubjects_Subject_Id",
+                table: "EnrolledSubjects",
+                column: "Subject_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnrolledSubjects_User_Id",
+                table: "EnrolledSubjects",
+                column: "User_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_InstructorId",
+                table: "Sessions",
+                column: "InstructorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_RoomId",
+                table: "Sessions",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_SubjectId",
+                table: "Sessions",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_CourseId",
+                table: "Subjects",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_SemesterId",
+                table: "Subjects",
+                column: "SemesterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_AssignedSectionId",
+                table: "User",
+                column: "AssignedSectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_User_Id",
+                table: "UserRoles",
+                column: "User_Id",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Applications");
+
+            migrationBuilder.DropTable(
+                name: "EnrolledProgram");
+
+            migrationBuilder.DropTable(
+                name: "EnrolledSubjects");
+
+            migrationBuilder.DropTable(
+                name: "Sessions");
+
+            migrationBuilder.DropTable(
+                name: "UserRoles");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+
+            migrationBuilder.DropTable(
+                name: "Subjects");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Semesters");
+
+            migrationBuilder.DropTable(
+                name: "Sections");
+
+            migrationBuilder.DropTable(
+                name: "Programs");
+        }
+    }
+}

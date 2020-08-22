dc89ea237d020ad7bc8aec336a7bea8c549f7000
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2020-08-22 16:29:47 +0800

    feat(Auth): Add Roles in JWT Sessions

--- src/eWAN.WebApi/UseCases/LogIn/LogInPresenter.cs
+++ src/eWAN.WebApi/UseCases/LogIn/LogInPresenter.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace eWAN.WebApi.UseCases.LogIn
 {
     using Application.Boundaries.LogIn;
     using Infrastructure.Auth;
+    using Domains.Role;
 
     public class LogInPresenter : ILogInOutputPort
     {
@@ -15,8 +19,14 @@ namespace eWAN.WebApi.UseCases.LogIn
         {
             string secret = System.Environment.GetEnvironmentVariable("SERVER_KEY");
 
-            var claims = new[] { new Claim(ClaimTypes.Name, output.user.Username) };
-            var identity = new ClaimsIdentity(claims);
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, output.user.Id));
+            foreach(var role in output.user.AssignedRoles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, ((int) role.role).ToString()));
+            }
+
+            var identity = new ClaimsIdentity(claims, JwtBearerDefaults.AuthenticationScheme);
 
             string token = IdentityToken.GenerateJwtSecurityToken(identity, secret);
 

a14ea88fae279bf484486706d9cdead9a9aedb1f
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2020-10-26 20:53:06 +0800

    chore: Move migrations

--- src/eWAN.Infrastructure/Database/Migrations/20201026124514_InitialMigration.cs
+++ src/eWAN.Infrastructure/Database/Migrations/20201026124514_InitialMigration.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace eWAN.Infrastructure.eWAN.Infrastructure.Database.Migrations
+namespace eWAN.Infrastructure.Database.Migrations
 {
     public partial class InitialMigration : Migration
     {

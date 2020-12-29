ac66507ef2237a8f68fee1211fc49e4895d6a1df
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2020-12-29 12:05:02 +0800

    chore(Ef): Add personal details

--- src/eWAN.Infrastructure/Database/Migrations/EwanIdentity/20201213135727_AddPersonalDetailsFields.Designer.cs
+++ src/eWAN.Infrastructure/Database/Migrations/EwanIdentity/20201213135727_AddPersonalDetailsFields.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eWAN.Infrastructure.Database;
 
 namespace eWAN.Infrastructure.Database.Migrations.EwanIdentity
 {
     [DbContext(typeof(EwanIdentityDbContext))]
-    partial class EwanIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201213135727_AddPersonalDetailsFields")]
+    partial class AddPersonalDetailsFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

35a79e54392789492c51ffffcbac1c2f10f33dbd
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-01-16 14:42:41 +0800

    feat: Add roles in seeding

--- src/eWAN.Identity/SeedData.cs
+++ src/eWAN.Identity/SeedData.cs
@@ -31,83 +31,138 @@ namespace eWAN.Identity
             {
                 using (var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
                 {
-                    var context = scope.ServiceProvider.GetService<EwanIdentityDbContext>();
-                    context.Database.Migrate();
+                    var roleMgr = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                    SeedData.InsertRoles(roleMgr);
+                    roleMgr.Dispose();
 
                     var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<eWAN.Infrastructure.Database.Entities.Identity>>();
-                    var alice = userMgr.FindByNameAsync("alice").Result;
-                    if (alice == null)
-                    {
-                        alice = new eWAN.Infrastructure.Database.Entities.Identity()
-                        {
-                            UserName = "alice",
-                            Email = "AliceSmith@email.com",
-                            EmailConfirmed = true,
-                            FirstName = "Alice",
-                            MiddleName = "Absr",
-                            LastName = "Smith",
-                            HomeAddress = "Bahay"
-                        };
-                        var result = userMgr.CreateAsync(alice, "Pass123$").Result;
-                        if (!result.Succeeded)
-                        {
-                            throw new Exception(result.Errors.First().Description);
-                        }
+                    SeedData.InsertUsers(userMgr);
+                    userMgr.Dispose();
+                }
+            }
+        }
 
-                        result = userMgr.AddClaimsAsync(alice, new Claim[]{
+        private static void InsertRoles(RoleManager<IdentityRole> roleManager)
+        {
+            var applicant = roleManager.FindByNameAsync("applicant").Result;
+            if(applicant == null)
+            {
+                applicant = new IdentityRole("applicant");
+
+                var result = roleManager.CreateAsync(applicant).Result;
+                if(!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+                Log.Debug("applicant role created");
+            }
+            else
+            {
+                Log.Debug("applicant already exists");
+            }
+
+            var student = roleManager.FindByNameAsync("student").Result;
+            if(student == null)
+            {
+                student = new IdentityRole("student");
+
+                var result = roleManager.CreateAsync(student).Result;
+                if(!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+                Log.Debug("student role created");
+            }
+            else
+            {
+                Log.Debug("student already exists");
+            }
+        }
+
+        private static void InsertUsers(UserManager<eWAN.Infrastructure.Database.Entities.Identity> userMgr)
+        {
+            var alice = userMgr.FindByNameAsync("alice").Result;
+            if (alice == null)
+            {
+                alice = new eWAN.Infrastructure.Database.Entities.Identity()
+                {
+                    UserName = "alice",
+                    Email = "AliceSmith@email.com",
+                    EmailConfirmed = true,
+                    FirstName = "Alice",
+                    MiddleName = "Absr",
+                    LastName = "Smith",
+                    HomeAddress = "Bahay"
+                };
+                var result = userMgr.CreateAsync(alice, "Pass123$").Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+
+                var roleResult = userMgr.AddToRoleAsync(alice, "student").Result;
+                if(!roleResult.Succeeded)
+                {
+                    throw new Exception(roleResult.Errors.First().Description);
+                }
+
+                result = userMgr.AddClaimsAsync(alice, new Claim[]{
                             new Claim(JwtClaimTypes.Name, "Alice Smith"),
                             new Claim(JwtClaimTypes.GivenName, "Alice"),
                             new Claim(JwtClaimTypes.FamilyName, "Smith"),
                             new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
                         }).Result;
-                        if (!result.Succeeded)
-                        {
-                            throw new Exception(result.Errors.First().Description);
-                        }
-                        Log.Debug("alice created");
-                    }
-                    else
-                    {
-                        Log.Debug("alice already exists");
-                    }
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+                Log.Debug("alice created");
+            }
+            else
+            {
+                Log.Debug("alice already exists");
+            }
 
-                    var bob = userMgr.FindByNameAsync("bob").Result;
-                    if (bob == null)
-                    {
-                        bob = new eWAN.Infrastructure.Database.Entities.Identity()
-                        {
-                            UserName = "bob",
-                            Email = "BobSmith@email.com",
-                            EmailConfirmed = true,
-                            FirstName = "Bob",
-                            MiddleName = "asda",
-                            LastName = "Smith",
-                            HomeAddress = "Bahay"
-                        };
-                        var result = userMgr.CreateAsync(bob, "Pass123$").Result;
-                        if (!result.Succeeded)
-                        {
-                            throw new Exception(result.Errors.First().Description);
-                        }
+            var bob = userMgr.FindByNameAsync("bob").Result;
+            if (bob == null)
+            {
+                bob = new eWAN.Infrastructure.Database.Entities.Identity()
+                {
+                    UserName = "bob",
+                    Email = "BobSmith@email.com",
+                    EmailConfirmed = true,
+                    FirstName = "Bob",
+                    MiddleName = "asda",
+                    LastName = "Smith",
+                    HomeAddress = "Bahay"
+                };
+                var result = userMgr.CreateAsync(bob, "Pass123$").Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+                var roleResult = userMgr.AddToRoleAsync(bob, "applicant").Result;
+                if(!roleResult.Succeeded)
+                {
+                    throw new Exception(roleResult.Errors.First().Description);
+                }
 
-                        result = userMgr.AddClaimsAsync(bob, new Claim[]{
+                result = userMgr.AddClaimsAsync(bob, new Claim[]{
                             new Claim(JwtClaimTypes.Name, "Bob Smith"),
                             new Claim(JwtClaimTypes.GivenName, "Bob"),
                             new Claim(JwtClaimTypes.FamilyName, "Smith"),
                             new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
                             new Claim("location", "somewhere")
                         }).Result;
-                        if (!result.Succeeded)
-                        {
-                            throw new Exception(result.Errors.First().Description);
-                        }
-                        Log.Debug("bob created");
-                    }
-                    else
-                    {
-                        Log.Debug("bob already exists");
-                    }
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
                 }
+                Log.Debug("bob created");
+            }
+            else
+            {
+                Log.Debug("bob already exists");
             }
         }
     }

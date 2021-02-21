dbf6582fd73cd0f7e49eecfbf3a1b8c08aa8294b
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-02-21 16:11:00 +0800

    chore: Rename project

--- Sinuka/src/Sinuka.Web/Startup.cs
+++ Sinuka/src/Sinuka.Web/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace src
+namespace Sinuka.Web
 {
     public class Startup
     {

345ce888b6be82dc8a21d048ed70daca2253ec7e
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-02-21 19:20:36 +0800

    chore(Sinuka): test config

---
+++ Sinuka/src/Sinuka.Web/Clients.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace Sinuka.Web
+{
+    internal class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "oauthClient",
+                    ClientName = "Example client application using client credentials",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = new List<Secret> {new Secret("SuperSecretPassword".Sha256())}, // change me!
+                    AllowedScopes = new List<string> {"api1.read"}
+                }
+            };
+        }
+    }
+}

a6b99b9b0c4a05d7ab7ef0a7fbf31dd346839d37
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-03-07 21:15:18 +0800

    chore(Sinuka): add migration

--- Sinuka/src/Sinuka.Core/Infrastructure/Database/Migrations/20210307131308_ReplaceIdentityModels.Designer.cs
+++ Sinuka/src/Sinuka.Core/Infrastructure/Database/Migrations/20210307131308_ReplaceIdentityModels.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sinuka.Core.Infrastructure.Database;
 
 namespace Sinuka.Core.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(SinukaDbContext))]
-    partial class SinukaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210307131308_ReplaceIdentityModels")]
+    partial class ReplaceIdentityModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Sinuka/src/Sinuka.Core/Infrastructure/Database/Migrations/20210307131308_ReplaceIdentityModels.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sinuka.Core.Infrastructure.Database.Migrations
+{
+    public partial class ReplaceIdentityModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "AspNetUserTokens",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "AspNetUserTokens",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "AspNetUserTokens",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "AspNetUserRoles",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "AspNetUserRoles",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "AspNetUserRoles",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "AspNetUserLogins",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "AspNetUserLogins",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "AspNetUserLogins",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "AspNetUserClaims",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "AspNetUserClaims",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "AspNetUserClaims",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "AspNetRoles",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "AspNetRoles",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "AspNetRoles",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "AspNetRoleClaims",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "AspNetRoleClaims",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "AspNetRoleClaims",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AspNetRoleClaims");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "AspNetRoleClaims");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "AspNetRoleClaims");
+        }
+    }
+}

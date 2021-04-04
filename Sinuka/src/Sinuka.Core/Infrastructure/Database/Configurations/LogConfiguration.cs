f9206f7fa170d0df57b19cbb8e3a57b952974a24
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-04-04 17:15:10 +0800

    chore: comment out Log

--- Sinuka/src/Sinuka.Core/Infrastructure/Database/Configurations/LogConfiguration.cs
+++ Sinuka/src/Sinuka.Core/Infrastructure/Database/Configurations/LogConfiguration.cs
@@ -4,11 +4,11 @@ using Sinuka.Core.Domains.Entities;
 
 namespace Sinuka.Core.Infrastructure.Database.Configurations
 {
-    public class LogConfiguration : IEntityTypeConfiguration<Sinuka.Core.Domains.Entities.Log>
-    {
-        public void Configure(EntityTypeBuilder<Log> builder)
-        {
-            builder.ToTable("Logs");
-        }
-    }
+    // public class LogConfiguration : IEntityTypeConfiguration<Sinuka.Core.Domains.Entities.Log>
+    // {
+    //     public void Configure(EntityTypeBuilder<Log> builder)
+    //     {
+    //         builder.ToTable("Logs");
+    //     }
+    // }
 }

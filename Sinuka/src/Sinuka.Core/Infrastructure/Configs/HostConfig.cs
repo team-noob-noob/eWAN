c796bab0b6e97745acc0fa0ed18257481ffd82db
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-04-10 18:13:06 +0800

    chore(Sinuka.Core): add accounts hosts

--- Sinuka/src/Sinuka.Core/Infrastructure/Configs/HostConfig.cs
+++ Sinuka/src/Sinuka.Core/Infrastructure/Configs/HostConfig.cs
@@ -3,5 +3,7 @@ namespace Sinuka.Core.Infrastructure.Configs
     public class HostConfig
     {
         public static string IdentityServerUrl = "https://localhost:5000";
+
+        public static string AccountsSyncUrl = "https://localhost:6000";
     }
 }

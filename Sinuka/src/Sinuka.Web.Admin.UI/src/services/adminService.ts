9a40448344232e7c053d0f236b6a0e3e27f3addd
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-04-26 17:53:12 +0800

    feat(Sinuka.Admin.UI): add reqs of admin APIs

--- Sinuka/src/Sinuka.Web.Admin.UI/src/services/adminService.ts
+++ Sinuka/src/Sinuka.Web.Admin.UI/src/services/adminService.ts
@@ -2,6 +2,41 @@ import axios from "axios";
 import { HOSTS } from "../config";
 
 export async function FetchTest() {
-    const response = await axios.get(`${HOSTS.ADMIN_APIS}/WeatherForecast`);
+    const response = await axios.get(`${HOSTS.ADMIN_APIS}/test/admin/WeatherForecast`);
     return response.data;
+}
+
+export async function FetchUsers(query: IUserQueryInputModel) {
+    const response = await axios.get(`${HOSTS.ADMIN_APIS}/Account/GetAllUsers`, {params: query});
+    return response.data;
+}
+
+export async function CreateUser(userData: INewUserInputModel) {
+    const response = await axios.post(`${HOSTS.ADMIN_APIS}/Account/CreateAccount`, userData);
+    return response.data;
+}
+
+export async function UpdateUser(userData: IUpdateUserInputModel) {
+    const response = await axios.put(`${HOSTS.ADMIN_APIS}/Account/UpdateUser`, userData);
+    return response.data;
+}
+
+interface IUserQueryInputModel {
+    Email: string;
+}
+
+interface INewUserInputModel {
+    Email: string;
+    Password: string;
+    Username: string;
+    PhoneNumber: string;
+}
+
+interface IUpdateUserInputModel {
+    Id: string;
+    Username: string | null;
+    Password: string | null;
+    Email: string | null;
+    PhoneNumber: string | null;
+    IsDeleted: boolean;
 }
